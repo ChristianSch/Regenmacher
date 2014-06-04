6005f8a45dc9c6b01ede4f0479ac63f856bfc455
Christian Schulze <christian.schulze@mni.thm.de>
2014-06-04 20:18:47 +0200

    Initial Commit

---
+++ Regenmacher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Regenmacher
+//
+//  Created by Christian Schulze on 04/06/14.
+//  Copyright (c) 2014 Christian Schulze. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Regenmacher/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Regenmacher
+//
+//  Created by Christian Schulze on 04/06/14.
+//  Copyright (c) 2014 Christian Schulze. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
